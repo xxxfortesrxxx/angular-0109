8da58a4be32a8e8e17c4906a8e23d4ba6238d6e1
xxxfortesrxxx <xxxfortesrxxx@gmail.com>
2026-02-09 11:16:51 +0800

    修正

--- src/app/@component/father/father.component.ts
+++ src/app/@component/father/father.component.ts
@@ -2,11 +2,11 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ExampleService } from './../../@service/example.service';
 import { Component, Input } from '@angular/core';
-import { ChildComponent } from '../child/child.component';
+
 
 @Component({
   selector: 'app-father',
-  imports: [ChildComponent, FormsModule],
+  imports: [ FormsModule],
   templateUrl: './father.component.html',
   styleUrl: './father.component.scss'
 })
